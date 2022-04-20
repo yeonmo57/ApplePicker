1607cb9586766e2820f5f197ad91c58ed165acb9
zerosik00 <zerosik00@unicomnet.co.kr>
2022-04-20 16:08:13 +0900

    gradle build

--- app/src/main/java/todo/swu/applepicker/MainActivity.java
+++ app/src/main/java/todo/swu/applepicker/MainActivity.java
@@ -3,6 +3,9 @@ package todo.swu.applepicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView) findViewById(R.id.textView);
+
+        Button DBBtn = (Button) findViewById(R.id.DBBtn) ;
+        DBBtn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Red") ;
+            }
+        });
     }
+
 }
