fe873333f2686fbff5a93f1597d1b8d6c31e580e
zerosik00 <zerosik00@unicomnet.co.kr>
2022-05-04 17:11:46 +0900

    ocr api connecting...

--- app/src/main/java/todo/swu/applepicker/MainActivity.java
+++ app/src/main/java/todo/swu/applepicker/MainActivity.java
@@ -1,10 +1,25 @@
 package todo.swu.applepicker;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.UUID;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import static android.content.ContentValues.TAG;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -20,13 +35,39 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    int testNum = 10;
+    StringBuffer response = new StringBuffer();
 
+    public void setResponse(StringBuffer res){
+        response = res;
+    }
+
+    public StringBuffer getResponse(){
+        return response;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("hi");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -34,8 +75,12 @@ public class MainActivity extends AppCompatActivity {
 
         final TextView textView = (TextView) findViewById(R.id.textView);
         final TextView readData = (TextView) findViewById(R.id.readData);
+        final TextView ocrData = (TextView) findViewById(R.id.ocrView);
+
         Button saveBtn = (Button) findViewById(R.id.saveBtn) ;
         Button readBtn = (Button) findViewById(R.id.readBtn) ;
+        Button ocrBtn = (Button) findViewById(R.id.ocrBtn) ;
+
 
 
                 readBtn.setOnClickListener(new Button.OnClickListener() {
@@ -85,13 +130,112 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
-        Thread thread = new Thread() {
-            public void run() {
-                OcrExam api = new OcrExam();
+
+        System.out.println("test");
+        String apiURL = "https://nxie8hhe8q.apigw.ntruss.com/custom/v1/15052/d9ca1275cb57d960372ef532fa2ff936a1f9e789c8af3e55617a0bd60c605d31/general";
+        String secretKey = "UUNjZ1p4ZXRMRk1VZlpramFkY1lwSFBrck5DS3hFdWw=";
+        String imageFile = "ocr_test01.png";
+        //C:\Users\USER\AndroidStudioProjects\ApplePicker\app\src\main\java\todo\swu\applepicker\ocr_test01.png
+
+
+        try {
+            URL url = new URL(apiURL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setUseCaches(false);
+            con.setDoInput(true);
+            con.setDoOutput(true);
+            con.setReadTimeout(30000);
+            con.setRequestMethod("POST");
+            String boundary = "----" + UUID.randomUUID().toString().replaceAll("-", "");
+            con.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+            con.setRequestProperty("X-OCR-SECRET", secretKey);
+
+            JSONObject json = new JSONObject();
+            json.put("version", "V2");
+            json.put("requestId", UUID.randomUUID().toString());
+            json.put("timestamp", System.currentTimeMillis());
+            JSONObject image = new JSONObject();
+            image.put("format", "jpg");
+            image.put("name", "demo");
+            JSONArray images = new JSONArray();
+            images.put(image);
+            json.put("images", images);
+            String postParams = json.toString();
+
+            con.connect();
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            long start = System.currentTimeMillis();
+            File file = new File(imageFile);
+            writeMultiPart(wr, postParams, file, boundary);
+            wr.close();
+
+            int responseCode = con.getResponseCode();
+            BufferedReader br;
+            if (responseCode == 200) {
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } else {
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
             }
-        };
-        thread.start();
+            String inputLine;
+            //StringBuffer response = new StringBuffer();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+            System.out.println("test");
+            System.out.println(response);
+            //setResponse(response);
+            ocrData.setText(response);
+            //testNum=200;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        ocrBtn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //ocrData.setText("test");
+                //ocrData.setText(getResponse());
+                //System.out.println(response);
+            }});
 
     }
+
+    private static void writeMultiPart(OutputStream out, String jsonMessage, File file, String boundary) throws
+            IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("--").append(boundary).append("\r\n");
+        sb.append("Content-Disposition:form-data; name=\"message\"\r\n\r\n");
+        sb.append(jsonMessage);
+        sb.append("\r\n");
+
+        out.write(sb.toString().getBytes("UTF-8"));
+        out.flush();
+
+        if (file != null && file.isFile()) {
+            out.write(("--" + boundary + "\r\n").getBytes("UTF-8"));
+            StringBuilder fileString = new StringBuilder();
+            fileString
+                    .append("Content-Disposition:form-data; name=\"file\"; filename=");
+            fileString.append("\"" + file.getName() + "\"\r\n");
+            fileString.append("Content-Type: application/octet-stream\r\n\r\n");
+            out.write(fileString.toString().getBytes("UTF-8"));
+            out.flush();
+
+            try (FileInputStream fis = new FileInputStream(file)) {
+                byte[] buffer = new byte[8192];
+                int count;
+                while ((count = fis.read(buffer)) != -1) {
+                    out.write(buffer, 0, count);
+                }
+                out.write("\r\n".getBytes());
+            }
+
+            out.write(("--" + boundary + "--\r\n").getBytes("UTF-8"));
+        }
+        out.flush();
+
+
+    }
+
 
 }
