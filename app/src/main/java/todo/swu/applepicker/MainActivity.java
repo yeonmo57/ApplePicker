419fab74fa0d065ff70f5054afd4fcb0de96c8c0
Kim YeonMo <ymthebest57@gmail.com>
2022-06-07 23:22:12 +0900

    0607 backend

--- app/src/main/java/todo/swu/applepicker/MainActivity.java
+++ app/src/main/java/todo/swu/applepicker/MainActivity.java
@@ -1,17 +1,24 @@
 package todo.swu.applepicker;
 
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.Manifest;
 import android.os.Bundle;
 
-import android.widget.DatePicker;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,19 +27,20 @@ public class MainActivity extends AppCompatActivity {
     Fragment fragmentOCR;
     TextView tv_title;
 
+    private FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        fragmentDaily = new FragmentDaily();
-        fragmentSNS = new FragmentSNS();
-        fragmentOCR = new FragmentOCR();
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MODE_PRIVATE);
+        Log.e(this.getClass().getName(), "mainActivity 실행");
 
         tv_title = (TextView)findViewById(R.id.tv_title);
+        fragmentDaily = new FragmentDaily();
 
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragmentDaily).commit();
+        fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.container, fragmentDaily, "fragment_daily").commit();
         // 초기화면 설정
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
@@ -40,21 +48,51 @@ public class MainActivity extends AppCompatActivity {
                 item -> {
                     switch (item.getItemId()) {
                         case R.id.tab_daily:
-                            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragmentDaily).commit();
+                            onMoveDaily();
                             tv_title.setText("Daily");
                             return true;
 
                         case R.id.tab_sns:
-                            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragmentSNS).commit();
+                            onMoveSNS();
                             tv_title.setText("SNS");
                             return true;
 
                         case R.id.tab_ocr:
-                            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragmentOCR).commit();
+                            onMoveOCR();
                             tv_title.setText("OCR");
                             return true;
                     }
                     return false;
                 });
+    }
+
+    private void onMoveDaily(){
+        if(fragmentDaily == null){
+            fragmentDaily = new FragmentDaily();
+            fragmentManager.beginTransaction().add(R.id.container, fragmentDaily, "fragment_daily").commit();
+        }
+        if(fragmentDaily != null)fragmentManager.beginTransaction().show(fragmentDaily).commit();
+        if(fragmentSNS != null)fragmentManager.beginTransaction().hide(fragmentSNS).commit();
+        if(fragmentOCR != null)fragmentManager.beginTransaction().hide(fragmentOCR).commit();
+    }
+
+    private void onMoveSNS(){
+        if(fragmentSNS == null){
+            fragmentSNS = new FragmentSNS();
+            fragmentManager.beginTransaction().add(R.id.container, fragmentSNS).commit();
+        }
+        if(fragmentDaily != null)fragmentManager.beginTransaction().hide(fragmentDaily).commit();
+        if(fragmentSNS != null)fragmentManager.beginTransaction().show(fragmentSNS).commit();
+        if(fragmentOCR != null)fragmentManager.beginTransaction().hide(fragmentOCR).commit();
+    }
+
+    private void onMoveOCR(){
+        if(fragmentOCR == null){
+            fragmentOCR = new FragmentOCR();
+            fragmentManager.beginTransaction().add(R.id.container, fragmentOCR).commit();
+        }
+        if(fragmentDaily != null)fragmentManager.beginTransaction().hide(fragmentDaily).commit();
+        if(fragmentSNS != null)fragmentManager.beginTransaction().hide(fragmentSNS).commit();
+        if(fragmentOCR != null)fragmentManager.beginTransaction().show(fragmentOCR).commit();
     }
 }
---
+++ app/src/main/java/todo/swu/applepicker/OcrEditActivity.java
@@ -0,0 +1,128 @@
+package todo.swu.applepicker;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Movie;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.button.MaterialButtonToggleGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class OcrEditActivity extends AppCompatActivity {
+    MaterialButtonToggleGroup toggle_btn_group;
+    ConstraintLayout bottom_sheet;
+    BottomSheetBehavior bottomSheetBehavior;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ocr_edit);
+
+        Intent receive_intent = getIntent();
+
+        String jsonResponse = receive_intent.getStringExtra("jsonResponse");
+        String imagePath = receive_intent.getStringExtra("imagePath");
+        List<String> inferTextList = new ArrayList<String>();
+        // OCR 글자들 모음 List
+        inferTextList = jsonParsing(jsonResponse);
+
+        Log.e(imagePath.toString(), "imagePath");
+        Log.e(inferTextList.toString(), "inferTextList");
+
+        // 이미지뷰에 이미지 띄우기
+        File imgFile = new  File(imagePath);
+
+        if(imgFile.exists()){
+            Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+            ImageView myImage = (ImageView) findViewById(R.id.iv_ocr_image);
+            myImage.setImageBitmap(myBitmap);
+        }
+
+        toggle_btn_group = (MaterialButtonToggleGroup) findViewById(R.id.toggle_btn_group_ocr_select);
+        toggle_btn_group.addOnButtonCheckedListener((toggle_btn_group, checkedId, isChecked) -> {
+            if (isChecked) {
+                switch (checkedId) {
+                    case R.id.btn_select_Task:
+                        //BottomSheet에 Task 부분에 선택한 (바운딩 박스 안의)문장이 더해지게 함
+                        break;
+
+                    case R.id.btn_select_Memo:
+                        //BottomSheet에 Memo 부분에 선택한 (바운딩 박스 안의)문장이 더해지게 함
+                        break;
+                }
+            }
+        });
+
+        bottom_sheet = findViewById(R.id.bottom_sheet);
+        bottomSheetBehavior = BottomSheetBehavior.from(bottom_sheet);
+
+
+        bottomSheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                //bottomSheet의 상태 변화에 따라 호출할 함수 지정
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                //bottomSheet의 펼쳐진 정도에 따른 동작 지정
+                // slideOffset 접힘 -> 펼쳐짐: 0.0 ~ 1.0
+            }
+        });
+
+    }
+    public String removeChar(String str, Integer n) {
+        String front = str.substring(0, n);
+        String back = str.substring(n+1, str.length());
+        return front + back;
+    }
+
+    private List jsonParsing(String json)
+    {
+        List<String> inferTextList = new ArrayList<String>();
+        Log.e("Test", "1");
+        try{
+            JSONObject imagesJsonObject = new JSONObject(json);
+            JSONArray imagesArray = imagesJsonObject.getJSONArray("images");
+
+            String imagesJson = imagesArray.toString();
+            imagesJson = removeChar(imagesJson, 0);
+            int len = imagesJson.length()-1;
+            imagesJson = removeChar(imagesJson, len);
+            Log.e(imagesArray.toString(), "imagesArray");
+
+            JSONObject fieldsJsonObject = new JSONObject(imagesJson);
+            JSONArray fieldsArray = fieldsJsonObject.getJSONArray("fields");
+
+            for(int i=0; i<fieldsArray.length(); i++)
+            {
+                JSONObject fieldsObject = fieldsArray.getJSONObject(i);
+                String inferText = fieldsObject.getString("inferText");
+
+                if(inferText.length()>=2)
+                    inferTextList.add(fieldsObject.getString("inferText"));
+            }
+        }catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return inferTextList;
+    }
+}
